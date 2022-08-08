09ba985dca178690907a2bf7bf2612a86f20faee
javenleung <914301050@qq.com>
2022-08-08 20:18:26 +0800

    feat: remove lib before build & remove redundant codes

--- src/alias-resolver.ts
+++ src/alias-resolver.ts
@@ -29,19 +29,6 @@ export function getFileManager(less: LessStatic): AliasResolverFileManagerContru
       super();
     }
 
-    renameFile(filename: string) {
-      const { aliasList } = this.options;
-      const alias = aliasList.find((item) => filename.startsWith(item.prefix));
-
-      if (alias) {
-        const { prefix, replaceText } = alias;
-        const subPath = filename.substring(prefix.length);
-        filename = resolve(replaceText, subPath.startsWith('/') ? `.${subPath}` : subPath);
-      }
-
-      return filename;
-    }
-
     supports(filename: string): boolean {
       const { aliasList } = this.options;
       return aliasList.some((item) => filename.startsWith(item.prefix));
